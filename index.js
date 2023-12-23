73eb75f08cc6c86b85688d2b565aec23d272ba65
Florent Quienne <133488794+FlorentQuienne42@users.noreply.github.com>
2023-12-23 22:50:08 +0100

    Add redirect

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Pouet pouet");
+console.log("Page loaded");
+const queryString = window.location.search;
+const parameters = new URLSearchParams(queryString);
+const videoNumber = +parameters.get('video');
+console.log("Redicecting to video n°", videoNumber);
+const videos = [
+    "https://www.youtube.com/watch?v=fw8FVpNfh1M",
+    "https://www.youtube.com/watch?v=8K2hoP3HojM"
+]
+document.location.href=videos[videoNumber - 1];
