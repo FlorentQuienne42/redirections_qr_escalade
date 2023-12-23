146154453b0b2045630a09e41e06440ca80f11da
Florent Quienne <133488794+FlorentQuienne42@users.noreply.github.com>
2023-12-23 22:55:03 +0100

    Only if defined

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ console.log("Page loaded");
 const queryString = window.location.search;
 const parameters = new URLSearchParams(queryString);
 const videoNumber = +parameters.get('video');
-console.log("Redicecting to video n°", videoNumber);
 const videos = [
     "https://www.youtube.com/watch?v=fw8FVpNfh1M",
     "https://www.youtube.com/watch?v=8K2hoP3HojM"
 ]
-document.location.href=videos[videoNumber - 1];
+const url = videos[videoNumber - 1];
+console.log("Redicecting to video n°", videoNumber, url);
+if (url) {
+    document.location.href = url;
+}
