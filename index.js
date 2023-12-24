7de70eb9b81d90e952aaf4fc5c99c7279ae8c308
Damscalator <49499858+Damscalator@users.noreply.github.com>
2023-12-24 01:11:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const queryString = window.location.search;
 const parameters = new URLSearchParams(queryString);
 const videoNumber = +parameters.get('video');
 const videos = [
-    "https://www.youtube.com/watch?v=fw8FVpNfh1M",
+    "https://www.youtube.com/watch?v=DN59pKJoF34",
     "https://www.youtube.com/watch?v=8K2hoP3HojM",
     "https://www.youtube.com/watch?v=4GRhBlqk15c"
 ]
